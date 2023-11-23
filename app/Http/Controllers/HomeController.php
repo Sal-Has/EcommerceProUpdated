390af5f20237650d8eb814896c811db62486a868
salehalkithiry <salehalkithiry03@gmail.com>
2023-11-23 15:43:51 +0300

    Saleh commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,8 @@ use App\Models\Product;
 
 use App\Models\Cart;
 
+use App\Models\Order;
+
 
 class HomeController extends Controller
 {
@@ -90,4 +92,75 @@ class HomeController extends Controller
             return redirect("login");
         }
     }
+        public function show_cart(){
+
+
+            if(Auth::id()){
+                $id = Auth::user()->id;
+
+                $cart=cart::where('user_id',"=",$id)->get();
+    
+                return view('home.showcart',compact("cart"));
+
+            }
+            else{
+
+                return redirect('login');
+            }
+        }
+        public function remove_cart($id){
+
+
+            $cart=cart::find($id);
+
+            $cart->delete();
+
+            return redirect()->back();
+
+        }
+
+
+public function cash_order(){
+
+    $user = Auth::user();
+
+    $userid = $user->id;
+
+
+    $data = cart::where("user_id","=",$userid)->get();
+
+    foreach($data as $data){
+        
+
+        $order = new order;
+
+        $order->name=$data->name;
+        $order->email=$data->email;
+        $order->phone=$data->phone;
+        $order->address=$data->address;
+        $order->user_id=$data->user_id;
+        $order->product_title=$data->product_title;
+        $order->quantity=$data->quantity;
+        $order->image=$data->image;
+        $order->product_id=$data->Product_id;
+
+        $order->payment_status="cash on delivery";
+
+        $order->delivery_status="processing";
+
+        $order->save();
+
+        $cart_id=$data->id;
+
+        $cart = cart::find($cart_id);
+
+        $cart->delete();
+
+
+
+    }
+
+    return redirect()->back()->with("message","We have receieved your order. We will connect with you soon");
+
+}
 }
